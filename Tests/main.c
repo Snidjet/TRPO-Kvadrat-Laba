22f9134f4ff47f49bf77021e8c5beeb44ba9d703
Бильская Анастасия Владимировна <ip513s05@awp11.csc.local>
2016-03-23 09:26:35 +0600

    dorabotali testi

--- Tests/main.c
+++ Tests/main.c
@@ -1,20 +1,24 @@
-#include <ifstream>
-#inclide <stdlib.h>
+#include <fstream>
+#include <stdlib.h>
 #include<stdio.h>
 #include "Kvad.c"
 #include<iostream>
 using namespace std;
-int main (int a,int b, int c) {
-  
-  int x1,x2,f,i;
-setlocate(LC ALL,"rus"); // отображение Кириллицы
-      
+int main () {
+  int a,b,c,f,i;
+  double x1,x2;
+
+  ofstream res("res.txt"); // файл результатов
+ 
+ 
   for (i=1; i<=2;i++) {
-ifstream test("test%d",i);
+ifstream test("test%c.txt",i);
 test>>a; test>>b; test>>c;
     
- f=Kvad(a,b,c,*x1,*x2);
- if (f==1) printf("test№%d=Complite \n",i); else printf("test№%d=Failed \n",i);
- }
-test.close();
+ f=Kvad(a,b,c,&x1,&x2);
+ if (f==1) res<<"test№"<<i<<"=Complite \n"; else res<<"test№"<<i<<"=Failed \n";
+ f=0;
+ res<<"123";
+  }
+
 }
